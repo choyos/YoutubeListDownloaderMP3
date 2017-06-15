2936b893b234086fd0795b35348bd1d92060efdd
CHM91 <cesar91hoyos@gmail.com>
2017-06-15 21:10:27 +0200

    Programa funcional con posibles bugs por caida de la pagina

--- main.py
+++ main.py
@@ -15,46 +15,70 @@ from bs4 import BeautifulSoup
 import requests
 
 ###### Get urls from youtube list ######
+
+list_link = raw_input('Inserte url de lista de youtube a descargar (pegar con Ctrl+Shit+V o con click derecho:\n')
+
 # Get request to youtube list
-req = requests.get("https://www.youtube.com/playlist?list=PLUY0yjvHWf2EWHtfV0x7F6JXpFLbgB_ah")
+req = requests.get(list_link)
 data = req.text
 soup = BeautifulSoup(data, "html.parser")
 
 # Prepare variables for loop
 song_links = []
-count = 0
 first = True
+header = "www.youtube.com"
 
 for link in soup.find_all('a'):
 	if link.get('href')[:9:1] == "/watch?v=":
 		if first == True:
 			song_links.append(link.get('href'))
 			first = False
-		elif link.get('href') != song_links[-1]:
-			song_links.append(link.get('href'))
-			count += 1
-# Remove list link
+		elif link.get('href') != song_links[-1][len(header)::]:
+			song_links.append(header + link.get('href'))
+
+# Remove list link and first link of page list image
+song_links.pop(0)
 song_links.pop(0)
 
-for link in song_links:
-	print link
 '''
-# Select browser, url and assert
+for link in song_links:
+	print link.split('&')[0]
+'''
+
+# Select browser (Chrome), url(mp3 downloader page) and assert(check everything right)
 driver = webdriver.Chrome()
 driver.get("http://www.youtube-mp3.org/es")
 assert "Convertidor YouTube a mp3" in driver.title
 
-win_handle = driver.current_window_handle
+# Get current handle to avoid pop-ups
+
 # Make magic
-# First page, input youtube query
-textinput = driver.find_element_by_id("youtube-url")
-textinput.clear()
-textinput.send_keys("https://www.youtube.com/watch?v=y8ogicK89T0")
-textinput.send_keys(Keys.RETURN)
+first = True
 
-driver.switch_to_window(win_handle)
-# Second page, click "Descargar" url
-download_link = driver.find_element_by_link_text("Descargar").click()
+for link in song_links:
+	if first == True:
+		win_handle = driver.current_window_handle
+		textinput = driver.find_element_by_id("youtube-url")
+		textinput.clear()
+		textinput.send_keys(link.split('&')[0])
+		textinput.send_keys(Keys.RETURN)
 
-#driver.close()
-'''
+		driver.switch_to_window(win_handle)
+		# Second page, click "Descargar" url
+		download_link = driver.find_element_by_link_text("Descargar").click()
+
+		first = False
+	else:
+		textinput = driver.find_element_by_id("youtube-url")
+		textinput.clear()
+		textinput.send_keys(link.split('&')[0])
+		textinput.send_keys(Keys.RETURN)
+
+		#driver.switch_to_window(win_handle)
+		time.sleep(1)
+		# Second page, click "Descargar" url
+		download_link = driver.find_element_by_link_text("Descargar").click()
+
+	time.sleep(1)
+
+driver.close()
